a2bb6b6fba9ee43ca797b52c00f9d30a5c87e542
Pedro Bacchini <pedrobacchini@outlook.com>
2019-07-23 10:06:32 -0300

    Initial Commit.

---
+++ src/test/java/com/github/pedrobacchini/springboottesting/SpringBootTestingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.pedrobacchini.springboottesting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootTestingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
